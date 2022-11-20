683b02355aa7c1778a0928ec7a782afa2c309f0b
papercore-dev <gyubini38@gmail.com>
2022-11-20 15:11:03 +0900

    Injeon Cloud 굴릴 수 있게 패치

---
+++ tailwindcss/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["/var/www/front/cloud/**/*.{html,js,php}", "/var/www/front/cloud/*.{html,js,php}", "/var/www/front/cloud/**/**/*.{html,js,php}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
